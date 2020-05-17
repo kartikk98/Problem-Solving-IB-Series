4653c2a5fcd4878310b88678c078f8e8cd81edc9
kartikk98 <kartik2189@gmail.com>
2020-05-17 16:15:53 +0530

    DP approach

--- Math/GridUniquePaths.cpp
+++ Math/GridUniquePaths.cpp
@@ -1,5 +1,31 @@
+// DP approach 
+// Time complexity : O(AB)
+// Space complexity : O(AB)
+int Solution::uniquePaths(int A, int B) {
+
+    vector<vector<int>> dp(A, vector<int> (B));
+
+    for(int i=0;i<A;i++)
+        dp[i][0] = 1;
+    for(int j=0;j<B;j++)
+        dp[0][j] = 1;
+
+    for(int i=1;i<A;i++)
+    {
+        for(int j=1;j<B;j++)
+        {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];  //only allowing down and right pathh if diagonal path allowed the +dp[i-1][j-1]
+        }
+    }
+    return dp[A-1][B-1];
+}
+
+
+// Recursuve approach with exponential time complexity
+/*
 int Solution::uniquePaths(int A, int B) {
     if(A==1 || B==1) return 1;
     
     return uniquePaths(A-1, B) + uniquePaths(A, B-1);
 }
+*/
